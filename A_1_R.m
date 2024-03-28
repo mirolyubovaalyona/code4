ade04f9128ea80dc75124ee14a06a3072f15f4fd
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2024-03-28 12:47:20 +1000

    1

---
+++ A_1_R.m
@@ -0,0 +1,7 @@
+function A = A_1_R( x,  w, k, o, t, H, fi)
+    v =(exp(-4/abs(w^2*(2*(x - 1)^2 - 1)^2 + o/k^2 + (2*t*(x - 1)^2)/k^2))*((abs(w^2*(2*(x - 1)^2 - 1)^2 + o/k^2 + (2*t*(x - 1)^2)/k^2)/4)^(1/2) - 1) + 1)^(1/2)
+    th= deval(H, x)
+    y= deval(fi, x)
+    fi= deval(fi, x)
+    A = - y(2)*sin(th(1))/(v*sin(th(1)-fi(1)))
+end
---
+++ diff_A_2_R.m
@@ -0,0 +1,7 @@
+function diff_A = diff_A_2_R( x,  w, k, o, t, H, fi)
+    v =(exp(-4/abs(w^2*((2*(x - 1/k^2)^2)/(1/k^2 - 1) - 1/k^2)^2 + o/k^2 + (t*((2*(x - 1/k^2)^2)/(1/k^2 - 1) - 1/k^2 + 1))/k^2))*((abs(w^2*((2*(x - 1/k^2)^2)/(1/k^2 - 1) - 1/k^2)^2 + o/k^2 + (t*((2*(x - 1/k^2)^2)/(1/k^2 - 1) - 1/k^2 + 1))/k^2)/4)^(1/2) - 1) + 1)^(1/2)
+    th= deval(H, x)
+    y= deval(fi, x)
+    fi= deval(fi, x)
+    diff_A = -v*y(2)*cos(th(1))*(sin(th(1)-fi(1))^(-1))
+end

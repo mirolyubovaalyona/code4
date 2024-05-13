87f646bf341d821808b698b04b4a5e059ba999d9
Shiqita <palchenko.dse@students.dvfy.ru>
2024-05-13 15:30:32 +1000

    Added W_n,m

--- final.m
+++ final.m
@@ -12,6 +12,13 @@ k2=0.5
 
 % указать самому из своих соображений
 
+%--------------------------------------
+sqr_ro = 1/k2; %ro^2
+ellipsoid_condition = 0; %0 - жЄсткий |1 - м€гкий
+%step=0.001;   % любой, желательно не ниже 0.001
+xi_3 = 3; % ksi_3*
+%--------------------------------------
+
 % n, m максимальные
 N_Max = 0
 M_Max = 0
@@ -29,6 +36,7 @@ e = 1e-5
 %точность метода –унге- утты 
 options_h_l = odeset('RelTol', 1e-5, 'AbsTol', 1e-7, 'Refine', 10)
 options_A = odeset('RelTol', 1e-5, 'AbsTol', 1e-7, 'Refine', 10)
+options_W = odeset('AbsTol',1e-06,'RelTol',1e-03,'Refine','8');
 
 %начало вычислений амплитуды рассе€ни€
 w=sqrt(w2)
@@ -40,24 +48,26 @@ term_1 = zeros(n_theta, n_fi)
 term_2 = zeros(n_theta, n_fi)   
 
 for n = 0:N_Max
-    e_1 = exp(2i*(-pi*(i1_1 + i2_1 + (1 - i3_1))/2 - pi*n));
-    e_2 = exp(2i*(-pi*(i1_2 + i2_2 + (1 - i3_2))/2 - pi*n));
+    e_1 = exp(2*i*(-pi*(i1_1 + i2_1 + (1 - i3_1))/2 - pi*n));
+    e_2 = exp(2*i*(-pi*(i1_2 + i2_2 + (1 - i3_2))/2 - pi*n));
     
     M_N = min(n, M_Max);
     
     for m = 0:M_N
         [h_1, l_1] = find_h_l(i1_1, i2_1, i3_1, w2, k2, w, k, m, n, delta_o, delta_t, e, options_h_l);
         [Xi_1_1, Xi_2_1] = find_A(h_1, l_1, i1_1, i2_1, i3_1, w2, k2, w, k, m, n, options_A, h_A);
+        w_xi3_1 = RWEF_13(sqr_ro,w2,l_1,h_1,i3_1,ellipsoid_condition,h_A,xi_3,options_W);
         Psi_1 = Xi_1_1(n_theta) * Xi_2_1(n_fi);
         
         [h_2, l_2] = find_h_l(i1_1, i2_1, i3_1, w2, k2, w, k, m, n, delta_o, delta_t, e, options_h_l);
         [Xi_1_2, Xi_2_2] = find_A(h_2, l_2, i1_2, i2_2, i3_2, w2, k2, w, k, m, n, options_A, h_A);
+        w_xi3_2 = RWEF_13(sqr_ro,w2,l_2,h_2,i3_2,ellipsoid_condition,h_A,xi_3,options_W);
         Psi_2 = Xi_1_2(n_theta) * Xi_2_2(n_fi);
         
         parfor q = 1:n_theta
             for j = 1:n_fi
-                term_1(q, j) = term_1(q, j) + (e_1 * Xi_1_1(q) * Xi_2_1(j) * Psi_1);
-                term_2(q, j) = term_2(q, j) + (e_2 * Xi_1_2(q) * Xi_2_2(j) * Psi_2);
+                term_1(q, j) = term_1(q, j) + (e_1 * Xi_1_1(q) * Xi_2_1(j) * Psi_1 *w_xi3_1);
+                term_2(q, j) = term_2(q, j) + (e_2 * Xi_1_2(q) * Xi_2_2(j) * Psi_2 *w_xi3_2);
             end
         end
     end
